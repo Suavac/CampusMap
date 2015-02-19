cfdb3ad024d548bf60eb8f4e1ad39af56e3725ca
daverickdunn <daverickdunn@gmail.com>
2015-02-19 11:25:26 +0000

    JSON Example added

---
+++ CampusMap/JSONTemplate.js
@@ -0,0 +1,81 @@
+/**
+ * Created by Richard on 19/02/2015.
+ */
+
+// If you look at 11:00am on our timetable you'll see two classes at once.
+// I'm gonna say we just skip this scenario for now, it's too much extra work
+// for proof of concept.
+
+response = {
+
+        "code" : "2BCT",
+        "title" : "B.Sc. in Computer Science & Information Technology",
+        "year" : "2014/2015",
+        "semester" : "Year 2, Semester 2",
+        "timetable" : [
+
+            {
+            "mod"   : "CT248",
+            "title" : "Introduction to Modelling",
+            "room"  : "ENG G017",
+            "lect"  : "H Melvin",
+            "lab"   : false,
+            "day"   : 1,
+            "time"  : 1},
+
+            {
+            "mod"   : "CT248",
+            "title" : "Introduction to Modelling",
+            "room"  : "IT106",
+            "lect"  : "H Melvin",
+            "lab"   : true,
+            "day"   : 1,
+            "time"  : 2},
+
+            {
+            "mod"   : "CT248",
+            "title" : "Introduction to Modelling",
+            "room"  : "IT106",
+            "lect"  : "H Melvin",
+            "lab"   : true,
+            "day"   : 1,
+            "time"  : 3},
+
+            {
+            "mod"   : "CT255",
+            "title" : "Next Generation Technology",
+            "room"  : "AC213",
+            "lect"  : "M Schukat",
+            "lab"   : false,
+            "day"   : 1,
+            "time"  : 4},
+
+            {
+            "mod"   : "CT255",
+            "title" : "Next Generation Technology",
+            "room"  : "AC213",
+            "lect"  : "M Schukat",
+            "lab"   : true,
+            "day"   : 1,
+            "time"  : 5},
+
+            {
+            "mod"   : "CT255",
+            "title" : "Next Generation Technology",
+            "room"  : "AC213",
+            "lect"  : "M Schukat",
+            "lab"   : true,
+            "day"   : 1,
+            "time"  : 6},
+
+            {
+            "mod"   : "CT255",
+            "title" : "Next Generation Technology",
+            "room"  : "IT125G",
+            "lect"  : null,
+            "lab"   : false,
+            "day"   : 1,
+            "time"  : 8},
+
+        ]
+    }
