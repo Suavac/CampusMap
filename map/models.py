d25349abc7b33e6e8edb21c75736a9876864cdfd
suavac <s.mlynarowicz1@nuigalway.ie>
2015-04-15 18:03:46 +0100

    Run on Python 2:
    By default PIN is located near Library... lon & lat removed from the Room Form

--- map/models.py
+++ map/models.py
@@ -43,8 +43,8 @@ class Room(models.Model):
     roomCode = models.CharField(max_length = 15, primary_key=True) # changed to 15
     roomName = models.CharField(max_length = 30)
     building = models.ForeignKey(Building)
-    lat = models.FloatField(max_length = 19)
-    lng = models.FloatField(max_length = 19)
+    lat = models.FloatField(max_length = 19, default= 53.27980291, editable=False)
+    lng = models.FloatField(max_length = 19, default=-9.06089351, editable=False)
 
     def __str__(self):
         return self.roomCode
