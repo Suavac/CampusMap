1c980866f02400733a0228550c7659d8919ebe04
suavac <s.mlynarowicz1@nuigalway.ie>
2015-04-17 20:02:47 +0100

    Fixed timetable filters not working in Chrome Added comments to and polished some more of the code

--- static/js/sm.js
+++ static/js/sm.js
@@ -1,18 +1,17 @@
 
+// function used for deleting records from database
 
-    var rowUniqueIdentifier;
+    function getRecordDetails( url ) {// parameter is the name of the template
 
-    function getRecordDetails( url ) {
-        var x;
         if (confirm("Are You Sure?") == true) { // confirm deletion
-            $(document).ready(function () {
+            $(document).ready(function () { // get values from the selected row (table residing on the template)
                 $('#test').on('click', 'tr', function (event) {
-                    var texts = $(this).children().map(function () {
+                        var rowUniqueIdentifier = $(this).children().map(function () {
                         return $.trim($(this).html())
                     }).get();
-                    rowUniqueIdentifier = texts[0];
-                    deleteRecord(url);
-                    location.reload();
+
+                           deleteRecord(url , rowUniqueIdentifier);
+
                 });
             });
         } else {
@@ -20,19 +19,21 @@
         }
     }
 
-    function deleteRecord( url ) {
+    function deleteRecord( url ,rowUniqueIdentifier ) {
 
         var request = new XMLHttpRequest();
+        if(url=='timetable') { // timetable records in uniquely identified by 3 values
+            var param = '../timetable/?message=delete' + '&course=' + rowUniqueIdentifier[0]+ '&year=' + rowUniqueIdentifier[1] + '&semester=' + rowUniqueIdentifier[2];
+        }
+        else{ // other table's records can be identified by their primary key
+            var param = '../'+url+'/?message=delete' + '&toDelete=' + rowUniqueIdentifier[0];
+        }
 
-        var param = '../'+url+'/?message=delete' + '&toDelete=' + rowUniqueIdentifier;
 
         request.open('GET', param, false);
         request.send(null);
+        location.reload();
 
-        if (request.responseText == 'success') {
-
-           console.log("message received")
-        }
 
     }
 
@@ -70,26 +71,3 @@
 
         document.getElementById('saveButton').disabled = false;
     }
-
-
-
-    function deleteTimetable() {
-
-       $(document).ready(function () {
-               $('#test').on('click', 'tr', function (event) {
-                   var texts = $(this).children().map(function () {
-                        return $.trim($(this).html())
-                   }).get();
-                   var course = texts[0];
-                   var year = texts[1];
-                   var semester = texts[2];
-                   var request = new XMLHttpRequest();
-
-                   var param = '../timetable/?message=delete' + '&course=' + course + '&year=' + year + '&semester=' + semester;
-
-                   request.open('GET', param, false);
-                   request.send(null);
-                   location.reload();
-               });
-          });
-       }
